b406cb6015493fbd47cb6564d74a3a43ea3bbc47
Marshall Offutt <marshalloffutt@gmail.com>
2018-09-19 21:55:56 -0500

    more practice

--- powers.js
+++ powers.js
@@ -1,10 +1,7 @@
-
-
 const handlerFunction = () => {
-    const toggler = document.getElementsByClassName("power disabled");
-    console.log('comment', toggler);
-    document.getElementById('flight').classList.toggle("enabled");
+    document.getElementById('flight').classList.replace( "power-disabled", "power-enabled");
  }
 
-
 document.querySelector("#activate-flight").addEventListener("click", handlerFunction);
+document.querySelector("#activate-mindreading").addEventListener("click", handlerFunction);
+document.querySelector("#activate-xray").addEventListener("click", handlerFunction);
